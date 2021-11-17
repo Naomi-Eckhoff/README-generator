c977e85ad45d1ccf38ab961b38fadd4a6123f683
Naomi-Eckhoff <nsuibhne@gmail.com>
2021-11-17 04:54:48 -0600

    laptop broke. files recovered from hard drive

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 
@@ -162,11 +163,16 @@ const readmeText = answersArray => {
 // TODO: Create a function to write README file
 //function writeToFile(fileName, data) { }
 
+const writeToFile = content => {
+
+}
+
 // TODO: Create a function to initialize app
 //function init() { }
 
 // Function call to initialize app
 //init();
 questions()
-  .then(readmeText);
+  .then(readmeText)
+  .then(writeToFile);
 
